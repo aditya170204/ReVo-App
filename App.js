60a8032d28ff9683d24ef66aee141496745ce84f
aditya170204 <adityaraj17feb@gmail.com>
2025-01-23 18:30:15 +0530

    added icons and more elements

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "./src/components/Dashboard";
-import Profile from "./src/components/Users";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
+
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Dashboard from "./src/components/Dashboard";
 import Users from "./src/components/Users";
@@ -11,16 +14,63 @@ import Teams from "./src/components/Teams";
 import Attendance from "./src/components/Attendance";
 import Leaves from "./src/components/Leaves";
 const App = () => {
-  const Stack = createNativeStackNavigator();
   const Tab = createBottomTabNavigator();
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="DASHBOARD" component={Dashboard} />
-        <Tab.Screen name="USERS" component={Users} />
-        <Tab.Screen name="TEAMS" component={Teams} />
-        <Tab.Screen name="ATTENDANCE" component={Attendance} />
-        <Tab.Screen name="LEAVES" component={Leaves} />
+      <Tab.Navigator
+        initialRouteName="DASHBOARD"
+        screenOptions={{ tabBarActiveColor: "#9A4D49" }}
+      >
+        <Tab.Screen
+          name="DASHBOARD"
+          component={Dashboard}
+          options={{
+            tabBarIcon: ({ color, focused, size }) => {
+              return <MaterialIcons name="dashboard" size={30} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="USERS"
+          component={Users}
+          options={{
+            tabBarIcon: ({ color, focused, size }) => {
+              return <FontAwesome5 name="users" size={25} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="TEAMS"
+          component={Teams}
+          options={{
+            tabBarIcon: ({ color, focussed, size }) => {
+              return <FontAwesome6 name="people-group" size={25} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="ATTENDANCE"
+          component={Attendance}
+          options={{
+            tabBarIcon: ({ color, focussed, size }) => {
+              return <FontAwesome6 name="calendar-check" size={25} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="LEAVES"
+          component={Leaves}
+          options={{
+            tabBarIcon: ({ color, focussed, size }) => {
+              return (
+                <MaterialCommunityIcons
+                  name="calendar-range-outline"
+                  size={30}
+                />
+              );
+            },
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,74 +1,396 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
 import React from "react";
 
 const Dashboard = () => {
   return (
-    <View style={styles.container}>
-      <View style={styles.profileContainer}>
-        <Text>Profile area</Text>
+    <ScrollView style={{ marginHorizontal: 20 }}>
+      <View
+        style={{
+          // width: "100%",
+          // alignItems: "center",
+          gap: 20,
+          // flex: 1,
+          // marginBottom: 500,
+        }}
+      >
+        <View style={{ alignItems: "center" }}>
+          <Text>Profile Area</Text>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            height: 300,
+            borderWidth: 1,
+            borderRadius: 20,
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              marginTop: 10,
+              fontFamily: "roboto",
+              fontSize: 25,
+              color: "#2B2B2B",
+              borderColor: "red",
+              borderWidth: 1,
+              marginBottom: 10,
+            }}
+          >
+            Employees Status
+          </Text>
+          <View
+            style={{ width: "60%", borderWidth: 1, borderColor: "#5D5D5D" }}
+          />
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: "row",
+              borderWidth: 1,
+              borderColor: "green",
+              width: "80%",
+              gap: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 90,
+                backgroundColor: "blue",
+                height: "85%",
+                width: "45%",
+              }}
+            >
+              <Text style={{ color: "white" }}>graph</Text>
+            </View>
+            <View
+              style={{
+                justifyContent: "center",
+                height: "75%",
+                width: "45%",
+                backgroundColor: "yellow",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "red",
+                    height: 10,
+                    width: 10,
+                    borderRadius: 10,
+                  }}
+                />
+                <Text>things on graph !</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "red",
+                    height: 10,
+                    width: 10,
+                    borderRadius: 10,
+                  }}
+                />
+                <Text>things on graph !</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "red",
+                    height: 10,
+                    width: 10,
+                    borderRadius: 10,
+                  }}
+                />
+                <Text>things on graph !</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "red",
+                    height: 10,
+                    width: 10,
+                    borderRadius: 10,
+                  }}
+                />
+                <Text>things on graph !</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            borderWidth: 1,
+            borderRadius: 20,
+            alignItems: "center",
+            rowGap: 20,
+            padding: 20,
+          }}
+        >
+          <View style={{ flexDirection: "row", columnGap: 60 }}>
+            <View
+              style={{
+                borderWidth: 1,
+                borderRadius: 10,
+                height: 40,
+                justifyContent: "center",
+              }}
+            >
+              <Text>employe score</Text>
+            </View>
+            <View
+              style={{
+                borderWidth: 1,
+                borderRadius: 10,
+                height: 40,
+                justifyContent: "center",
+              }}
+            >
+              <Text>employe score</Text>
+            </View>
+          </View>
+          <View style={{ flexDirection: "row", columnGap: 60 }}>
+            <View
+              style={{
+                borderWidth: 1,
+                borderRadius: 10,
+                height: 40,
+                justifyContent: "center",
+              }}
+            >
+              <Text>employe score</Text>
+            </View>
+            <View
+              style={{
+                borderWidth: 1,
+                borderRadius: 10,
+                height: 40,
+                justifyContent: "center",
+              }}
+            >
+              <Text>employe score</Text>
+            </View>
+          </View>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            borderWidth: 1,
+            borderRadius: 20,
+            alignItems: "center",
+          }}
+        >
+          <Text
+            style={{
+              marginTop: 10,
+              fontFamily: "roboto",
+              fontSize: 25,
+              color: "#2B2B2B",
+              borderColor: "red",
+              borderWidth: 1,
+              marginBottom: 10,
+            }}
+          >
+            Working Days This Year
+          </Text>
+          <View
+            style={{ width: "60%", borderWidth: 1, borderColor: "green" }}
+          />
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: "row",
+              borderWidth: 1,
+              borderColor: "green",
+              width: "80%",
+              gap: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 100,
+                backgroundColor: "blue",
+                height: 300,
+                width: "45%",
+              }}
+            >
+              <Text style={{ color: "white" }}>graph</Text>
+            </View>
+
+            <View
+              style={{
+                justifyContent: "center",
+                height: 100,
+                width: "45%",
+                backgroundColor: "yellow",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "red",
+                    height: 10,
+                    width: 10,
+                    borderRadius: 10,
+                  }}
+                />
+                <Text>things on graph !</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "red",
+                    height: 10,
+                    width: 10,
+                    borderRadius: 10,
+                  }}
+                />
+                <Text>things on graph !</Text>
+              </View>
+            </View>
+          </View>
+          <Text
+            style={{
+              marginTop: 10,
+              fontFamily: "roboto",
+              fontSize: 25,
+              color: "#2B2B2B",
+              borderColor: "red",
+              borderWidth: 1,
+              marginBottom: 10,
+            }}
+          >
+            Working Days This Month
+          </Text>
+          <View
+            style={{ width: "60%", borderWidth: 1, borderColor: "#5D5D5D" }}
+          />
+          <View
+            style={{
+              marginTop: 20,
+              flexDirection: "row",
+              borderWidth: 1,
+              borderColor: "green",
+              width: "80%",
+              gap: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "center",
+                borderRadius: 90,
+                backgroundColor: "blue",
+                height: "100",
+                width: "45%",
+              }}
+            >
+              <Text style={{ color: "white" }}>graph</Text>
+            </View>
+            <View
+              style={{
+                justifyContent: "center",
+                height: 200,
+                width: "45%",
+                backgroundColor: "yellow",
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "red",
+                    height: 10,
+                    width: 10,
+                    borderRadius: 10,
+                  }}
+                />
+                <Text>things on graph !</Text>
+              </View>
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: "red",
+                    height: 10,
+                    width: 10,
+                    borderRadius: 10,
+                  }}
+                />
+                <Text>things on graph !</Text>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
-      <View style={styles.divider}></View>
-      <View style={styles.contentContainer}>
-        <View style={styles.employeStatus}>
-          <Text>employe Status</Text>
-        </View>
-        <View style={styles.employeScore}>
-          <Text>employeScore</Text>
-        </View>
-        <View style={styles.workingDays}>
-          <Text>workingDays</Text>
-        </View>
-      </View>
-    </View>
+    </ScrollView>
   );
 };
 
 export default Dashboard;
 
-const styles = StyleSheet.create({
-  container: { flex: 1, alignItems: "center" },
-  profileContainer: {
-    width: "90%",
-    height: "15%",
-    margin: "3%",
-  },
-  divider: {
-    width: "90%",
-    borderWidth: 1,
-    borderColor: "#5D5D5D",
-  },
-  contentContainer: {
-    height: "100%",
-    width: "90%",
-    marginTop: 10,
-  },
-  employeStatus: {
-    width: "100%",
-    height: "35%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#5D5D5D",
-    borderWidth: 1,
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-  employeScore: {
-    width: "100%",
-    height: "25%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#5D5D5D",
-    borderWidth: 1,
-    borderRadius: 10,
-  },
-  workingDays: {
-    width: "100%",
-    height: "35%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#5D5D5D",
-    borderWidth: 1,
-    borderRadius: 10,
-    marginBottom: 10,
-  },
-});
+const styles = StyleSheet.create({});
